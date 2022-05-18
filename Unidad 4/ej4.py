b9dd145c2683ca5dc6d2a644fcd5b2ef8b35ced2
dnogues89 <dnogues@MacBook-Pro-de-Damian.local>
2022-05-17 21:36:25 -0300

    Unidad

---
+++ Unidad 4/ej4.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 4 – Dificultad alta
+Crear una función lambda que tome como parámetro una frase y la escriba al revés. 
+"""
+
+reves = lambda string: "".join(reversed(string))
+
+print(reves("HOLA"))
--- Unidad 4/ej5.py
+++ Unidad 4/ej5.py
@@ -1,8 +1,21 @@
 """
-Ejercicio 4 – Dificultad alta
-Crear una función lambda que tome como parámetro una frase y la escriba al revés. 
+Ejercicio 5 – Dificultad media
+Cree un programa que utilizando una función, solicite la edad de la persona e imprima todos los años que la persona ha cumplido según el siguiente formato de ejemplo. 
+1, 2, 3, 4, 5
+Y 
+5, 4, 3, 2, 1
+
 """
 
-reves = lambda string: "".join(reversed(string))
 
-print(reves("HOLA"))
+def edad():
+    age = int(input("Cuantos años tenes?\n"))
+    cumples = []
+    cumple_inv = [0]
+    for i in range(1, age + 1):
+        cumples += [i]
+
+    return f"{cumples}\n{cumple_inv}"
+
+
+print(edad())
