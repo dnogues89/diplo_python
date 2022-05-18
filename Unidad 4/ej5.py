81be79057b544c2342d9f0b656c26e105bfe8470
Damian Nogues <72810637+dnogues89@users.noreply.github.com>
2022-05-17 21:35:21 -0300

    Delete ej5.py

--- Unidad 4/ej5.py
+++
@@ -1,8 +0,0 @@
-"""
-Ejercicio 4 – Dificultad alta
-Crear una función lambda que tome como parámetro una frase y la escriba al revés. 
-"""
-
-reves = lambda string: "".join(reversed(string))
-
-print(reves("HOLA"))
