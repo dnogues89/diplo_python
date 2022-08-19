a34533b52a53b73553e31db387876c24ceb63f5e
ArielDemarco <ariel.demarco@pedidosya.com>
2022-08-19 14:55:00 -0300

    Made controller an instantiable working object

--- evaluacion 24-8/__main__.py
+++ evaluacion 24-8/__main__.py
@@ -1,6 +1,5 @@
 from src.controlador import Aplicacion
 
-if __name__ == "__main__":
-    mi_app = Aplicacion()
-    while mi_app.run():
-        pass
+mi_app = Aplicacion()
+while mi_app.run():
+    pass
--- evaluacion 24-8/src/controlador.py
+++ evaluacion 24-8/src/controlador.py
@@ -4,44 +4,48 @@ import re
 
 
 class Aplicacion:
+    def __init__(self):
+        self.vista = Visual()
+        self.repositorio = Repositorio()
+
+
     def run(self):
         patron = "[0-9]+"
-        opcion = Visual().menu()
+        opcion = self.vista.menu()
         if re.match(patron, str(opcion)):
-
             if opcion == 1:
                 modelo, cantidad = input(
                     "Ingresar Modelo y cantidad (Separado con un Espacio): "
                 ).split(" ")
-                Repositorio().alta(modelo, int(cantidad))
+                self.repositorio.alta(modelo, int(cantidad))
                 return True
 
             if opcion == 2:
-                lista = Repositorio().consulta()
-                Visual().lista(lista)
+                lista = self.repositorio.consulta()
+                self.vista.lista(lista)
                 id = int(input("Ingresar el ID del producto a eliminar. "))
-                Repositorio().baja(id)
+                self.repositorio.baja(id)
                 return True
 
             if opcion == 3:
-                lista = Repositorio().consulta()
-                Visual().lista(lista)
+                lista = self.repositorio.consulta()
+                self.vista.lista(lista)
                 return True
 
             if opcion == 4:
-                lista = Repositorio().consulta()
-                Visual().lista(lista)
+                lista = self.repositorio.consulta()
+                self.vista.lista(lista)
                 id, modelo, cantidad = input(
                     "Ingresar ID a moificar Modelo y cantidad (Separado con un Espacio): "
                 ).split(" ")
-                Repositorio().modificar(int(id), modelo, int(cantidad))
+                self.repositorio.modificar(int(id), modelo, int(cantidad))
                 print("Modificacion realizada con exito")
-                lista = Repositorio().consulta()
-                Visual().lista(lista)
+                lista = self.repositorio.consulta()
+                self.vista.lista(lista)
                 return True
 
             if opcion == 5:
-                Visual().exit()
+                self.vista.exit()
                 return False
 
         print("El numero ingresado es incorrecto")
