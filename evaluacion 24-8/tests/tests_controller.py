993f4a85160e000aa781f816f4cce184ceb19c59
ArielDemarco <ariel.demarco@pedidosya.com>
2022-08-19 15:59:58 -0300

    Added tests for option 3

--- evaluacion 24-8/tests/tests_controller.py
+++ evaluacion 24-8/tests/tests_controller.py
@@ -2,17 +2,25 @@ from src import controlador
 import unittest
 from unittest.mock import patch
 
+from src.vista import Visual
+
 
 class VistaMock:
     def __init__(self, menu_value: int):
         self.value = menu_value
         self.mostrar_error_fue_invocado = False
+        self.lista_lista_recibida = None
+        self.lista_fue_invocado = False
 
     def menu(self):
         return self.value
 
     def mostrar_error(self):
         self.mostrar_error_fue_invocado = True
+    
+    def lista(self, lista):
+        self.lista_lista_recibida = lista
+        self.lista_fue_invocado = True
 
 class RepositorioSpy:
     def __init__(self):
@@ -21,6 +29,12 @@ class RepositorioSpy:
     def alta(self, modelo, cantidad):
         self.alta_fue_invocado = True
 
+class RepositorioMock: 
+    def __init__(self, stubbed_data = []):
+        self.stubbed_data = stubbed_data
+
+    def consulta(self):
+        return self.stubbed_data
 
 class TestController(unittest.TestCase):
     @patch('builtins.input', lambda *args: 'abcde 123')
@@ -40,3 +54,12 @@ class TestController(unittest.TestCase):
         return_value = app.run()
         self.assertTrue(return_value)
         self.assertTrue(vista_que_retorna_opcion_1.mostrar_error_fue_invocado)
+
+    def test_option3_devuelve_toda_la_informacion_de_base_de_datos_a_la_vista(self):
+        visual = VistaMock(menu_value=3)
+        data_de_stock = [1, "abc", 123]
+        repositorio = RepositorioMock(stubbed_data=data_de_stock)
+        app = controlador.Aplicacion(visual=visual, repositorio=repositorio)
+        app.run()
+        self.assertTrue(visual.lista_fue_invocado)
+        self.assertEqual(visual.lista_lista_recibida, data_de_stock)
