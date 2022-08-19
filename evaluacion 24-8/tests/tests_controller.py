0643cb04eeeb12c1d05918768f06d6088d458c23
ArielDemarco <ariel.demarco@pedidosya.com>
2022-08-19 15:19:40 -0300

    First test done

--- evaluacion 24-8/tests/tests_controller.py
+++ evaluacion 24-8/tests/tests_controller.py
@@ -1,5 +1,29 @@
+from src import controlador
 import unittest
+from unittest.mock import patch
+
+class VistaMock:
+    def __init__(self, menu_value: int):
+        self.value = menu_value
+
+    def menu(self):
+        return self.value
+
+class RepositorioSpy:
+    def __init__(self):
+        self.alta_fue_invocado = False
+
+    def alta(self, modelo, cantidad):
+        self.alta_fue_invocado = True
+
 
 class TestController(unittest.TestCase):
-    def test(self):
-        pass
+    @patch('builtins.input', lambda *args: 'abcde 123')
+    def test_option1_al_agregar_modelo_y_cantidad_se_da__de_alta_en_repositorio(self):
+        repositorio = RepositorioSpy()
+        vista_que_retorna_opcion_1 = VistaMock(menu_value=1)
+        app = controlador.Aplicacion(visual=vista_que_retorna_opcion_1, repositorio=repositorio)
+        return_value = app.run()
+        self.assertTrue(repositorio.alta_fue_invocado)
+        self.assertTrue(return_value)
+
